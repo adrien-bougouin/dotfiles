9b1a4d9f6d071f3a85ade188873edad4b28f5135
Adrien Bougouin <adrien.bougouin@gmail.com>
2022-12-06 21:55:05 +0900

    adjust nvim key bindings

--- home/nvim/.config/nvim/lua/key_bindings.lua
+++ home/nvim/.config/nvim/lua/key_bindings.lua
@@ -1,12 +1,10 @@
--- :help mapleader
 -- :help lua-keymap
 
 vim.g.mapleader = ' ' -- <leader> = <space>
 
 -- Tab management.
 vim.keymap.set('n', '<C-t>', ':tabnew<cr>', {})
-vim.keymap.set('n', '<C-[>', ':tabprevious<cr>', {})
-vim.keymap.set('n', '<C-]>', ':tabnext<cr>', {})
+vim.keymap.set('i', '<C-t>', '<esc>:tabnew<cr>', {})
 
 -- System clipboard management.
 vim.keymap.set('v', '<C-S-c>', '"*y', {})
@@ -14,10 +12,15 @@ vim.keymap.set('i', '<C-S-v>', '<esc>"*pa', {})
 
 -- FZF plugin controls.
 if pcall(require, 'fzf-lua') then
-  vim.keymap.set('n', '<leader>f', ':FzfLua files<cr>', {})
-  vim.keymap.set('n', '<leader><S-f>', ':FzfLua git_files<cr>', {})
-  vim.keymap.set('n', '<leader>b', ':FzfLua buffers<cr>', {})
-  vim.keymap.set('n', '<leader>g', ':FzfLua live_grep_resume<cr>', {})
+  vim.keymap.set('n', '<C-f>', ':FzfLua files<cr>', {})
+  vim.keymap.set('n', '<C-S-f>', ':FzfLua git_files<cr>', {})
+  vim.keymap.set('n', '<C-b>', ':FzfLua buffers<cr>', {})
+  vim.keymap.set('n', '<C-g>', ':FzfLua live_grep_resume<cr>', {})
+
+  vim.keymap.set('i', '<C-f>', '<esc>:FzfLua files<cr>', {})
+  vim.keymap.set('i', '<C-S-f>', '<esc>:FzfLua git_files<cr>', {})
+  vim.keymap.set('i', '<C-b>', '<esc>:FzfLua buffers<cr>', {})
+  vim.keymap.set('i', '<C-g>', '<esc>:FzfLua live_grep_resume<cr>', {})
 else
   vim.notify("'fzf-lua' plugin not found. Fuzzy finding key bindings won't work.")
 end
