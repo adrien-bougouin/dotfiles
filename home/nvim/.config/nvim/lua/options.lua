264ee5d7e09cd2b2bdaa5227b8f001c5165e1c15
Adrien Bougouin <adrien.bougouin@gmail.com>
2025-09-21 18:18:30 +0900

    reorganiza options

--- home/nvim/.config/nvim/lua/options.lua
+++ home/nvim/.config/nvim/lua/options.lua
@@ -1,25 +1,27 @@
 -- :help lua-vim-opt
 
-vim.opt.background     = "light"
-vim.opt.termguicolors  = false
+-- Encoding
+vim.opt.fileencoding = "utf-8"
+vim.opt.fileformats = "unix,mac,dos"
 
-vim.opt.cursorline     = true
-vim.opt.mouse          = "a"
-vim.opt.number         = true
+-- UI
+vim.opt.background = "light"
+vim.opt.cursorline = true
+vim.opt.incsearch = true
+vim.opt.mouse = "a"
+vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.ruler          = true
+vim.opt.ruler = true
+vim.opt.showmatch = true
+vim.opt.termguicolors = false
 
-vim.opt.backspace      = "indent,eol,start"
-vim.opt.fileencoding   = "utf-8"
-vim.opt.fileformats    = "unix,mac,dos"
+-- Indentation
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smarttab = true
+vim.opt.softtabstop = 2
+vim.opt.tabstop = 2
 
-vim.opt.history        = 100
-vim.opt.incsearch      = true
-vim.opt.showmatch      = true
-
-vim.opt.autoindent     = true
-vim.opt.expandtab      = true
-vim.opt.shiftwidth     = 2
-vim.opt.smarttab       = true
-vim.opt.softtabstop    = 2
-vim.opt.tabstop        = 2
+-- Backspace behavior
+vim.opt.backspace = "indent,eol,start"
